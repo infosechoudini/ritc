96f73475cfe0e1ab54adc4150f2ad17ad8f94050
Harry Thomas <harry@infosechoudini.com>
2022-05-02 22:34:41 -0400

    Update mod.rs

--- src/arch/linux-x86_64/mod.rs
+++ src/arch/linux-x86_64/mod.rs
@@ -1,12 +1,3 @@
-// Copyright 2022 @infosechoudini. See the
-// COPYRIGHT file at the top-level directory of this distribution.
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
 //! This library was built for x86-64 Linux.
 
 use core::arch::asm;
